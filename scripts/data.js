e7bab3d065d16e0e357522c5b182bfff69bff821
Mikail Celikbas <mikail.celikbas@gmail.com>
2019-04-18 11:58:02 +0200

    calculate grades for subjects at 100%

--- scripts/data.js
+++ scripts/data.js
@@ -1,43 +1,67 @@
 var section_data = [
     {
-        "title": "title 1",
+        "title": "JavaScript",
         "content":
             [
                 {
-                    subtitle: "sth 1",
-                    progress: "80"
-                }
-            ]
-    },
-    {
-        "title": "title 2",
-        "content":
-            [
-                {
-                    subtitle: "sth 1",
+                    subtitle: "Syntaxe et éléments de base du javascript",
                     progress: "100"
                 },
                 {
-                    subtitle: "sth 2",
-                    progress: "20"
+                    subtitle: "Les fonctions et objets personnalisés",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Les tableaux",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Manipuler des objets",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Portée des données",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Manipulation du DOM via jquery",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Requête Ajax",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Utiliser au moins une librairie",
+                    progress: "100"
+                },
+                {
+                    subtitle: "Utiliser npm",
+                    progress: "100"
+                },
+            ]
+    },
+    {
+        "title": "PHP",
+        "content":
+            [
+                {
+                    subtitle: "Faire un CRUD",
+                    progress: "100"
                 }
             ]
     },
     {
-        "title": "title 3",
+        "title": "Symfony",
         "content": 
         [
             {
-                subtitle: "sth 1",
-                progress: "10"
+                subtitle: "Première page avec symfony",
+                progress: "0"
             },
             {
-                subtitle: "sth 2",
-                progress: "20"
-            },
-            {
-                subtitle: "sth 3",
-                progress: "30"
+                subtitle: "Doctrine",
+                progress: "0"
             }
         ]
     }
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,15 @@
 $(function() {
     
-    $.each(section_data, function (i, val) {
-        var tmpl = $("#tmpl_section").html();
-        var output = Mustache.render(tmpl, val);
-        $("#div_section").append(output);
-    });
-    
-    $.each($(".progressBarFill"), function (i, val) {
-        var str = $(this).text();
-        moveProgressBar($(val), str.substring(0, str.length - 1));
-    });
-    
+    user = {
+        firstname: "Bob",
+        lastname: "Kelso",
+        group: "13"
+    }
+
+    renderUserInfo(user);
+    renderSections();
+    fillProgressBars();
+
     //$(".content").toggle();
     $(".more-less").click(function () {
         $(this).parents("section").find(".content").animate({
@@ -34,9 +33,42 @@ $(function() {
         $("#div_note").find("p").text("Note: " + calculateGrade());
     });
 
-    // date & time
+    // set date & time update rate to one second
     setInterval(updateDateAndTime, 1000);
 });
+
+
+function renderUserInfo(user){
+    var tmpl = $("#tmpl_user_info").html();
+    var output = Mustache.render(tmpl, user);
+    $("#div_user_info").append(output);
+}
+
+function renderSections() {
+    $.each(section_data, function (i, val) {
+        var tmpl = $("#tmpl_section").html();
+        var output = Mustache.render(tmpl, val);
+        $("#div_section").append(output);
+    });
+}
+
+function fillProgressBars(){
+    $.each($(".progressBarFill"), function (i) {
+        var nb = $(this).find('strong').text();
+        if (nb < 5) {
+            $(this).html('<strong>' + nb + '</strong>');
+        }
+        else if (nb < 35) {
+            $(this).html('<strong>' + nb + '</strong> %');
+        }
+        moveProgressBar($(this), nb);
+        if (nb == 100) {
+            // we remove class d-none of bootstrap bc compatibility issue with jquery
+            // we go up 2 parents in the DOM then acces the previous sibling, and  thenfind the class "div_note"
+            $(this).parent().parent().prev().find(".div_note").removeClass("d-none");
+        }
+    });
+}
 
 function moveProgressBar(bar, targetWidth) {
     var width = 0;
@@ -50,14 +82,21 @@ function moveProgressBar(bar, targetWidth) {
     }
 }
 
+
+
 function calculateGrade() {
     let grade = 0;
+    let nb_grade = 0;
     $(".range-field").each(function (i, value) {
-        grade += parseInt($(value).val());
+        if(!$(this).parent().parent().parent().hasClass("d-none")) {
+            grade += parseInt($(value).val());
+            nb_grade++;
+        }
     });
-    return Math.round((grade / $(".range-field").length) * 100) / 100;
+    return Math.round((grade / nb_grade) * 100) / 100;
 }
 
+// ajax call for getting date & time (note: time is GMT-2)
 function updateDateAndTime() {
     $.getJSON("http://date.jsontest.com/", function (data) {
         var tmpl = $("#tmpl_date").html();
